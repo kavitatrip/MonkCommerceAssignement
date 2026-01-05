16e5437068c57da56f1e34ca8a8dd84ea8622f39
Kavita <kavitatripathi1998@gmail.com>
2026-01-05 22:03:36 +0530

    routerchanges

--- ecommapp/src/App.jsx
+++ ecommapp/src/App.jsx
@@ -1,13 +1,13 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import MainContainer from "./components/MainPage";
 
 function App() {
   return (
-    <HashRouter>
+    <BrowserRouter>
       <Routes>
         <Route path="/" element={<MainContainer />} />
       </Routes>
-    </HashRouter>
+    </BrowserRouter>
   );
 }
 
